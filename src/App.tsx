d7b607b5a7c54b5cc00320971ac4d046cc33aee8
MikkoKarvonen <mkarvonen34@gmail.com>
2024-09-09 16:38:18 +0300

    Add button to select location

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import data from "./jsons/2.json";
 
 function App() {
+  const [selectedLocation, setSelectedLocation] = useState(0);
+
   return (
     <div>
-      <h1>
-        {data.location}. {data.name}
-      </h1>
-      <p>{data.data.text}</p>
+      {selectedLocation === 0 ? (
+        <button onClick={() => setSelectedLocation(2)}>2</button>
+      ) : (
+        <div>
+          <h1>
+            {data.location}. {data.name}
+          </h1>
+          <p>{data.data.text}</p>
+          <button onClick={() => setSelectedLocation(0)}>0</button>
+        </div>
+      )}
     </div>
   );
 }

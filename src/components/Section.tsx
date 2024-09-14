0072925c093e62570b811ddd624d175401f02791
MikkoKarvonen <mkarvonen34@gmail.com>
2024-09-14 08:42:41 +0300

    Add linking to sections

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface locationProps {
   location: {
     location: number;
@@ -7,7 +9,7 @@ interface locationProps {
       text?: string;
       condition?: {
         name?: string;
-        section?: number;
+        section?: string;
         text?: string;
       }[];
     }[];
@@ -16,31 +18,51 @@ interface locationProps {
 }
 
 function Section({ location, setSelectedLocation }: locationProps) {
+  const [sectionNumber, setSectionNumber] = useState(0);
+
+  const section = location.data[sectionNumber];
+
+  const goToSection = (section?: string) => {
+    if (section) {
+      const [loc, sec] = section.split(".");
+      setSelectedLocation(parseInt(loc));
+      setSectionNumber(parseInt(sec));
+    }
+  };
+
   return (
     <div>
       <h1>
         {location.location}. {location.name}
       </h1>
-      {location.data.map((item, index) => (
-        <div key={index}>
-          {item.text ? (
-            <p key={item.index}>{item.text}</p>
-          ) : (
-            item.condition?.map((condition, conditionIndex) => (
-              <div key={conditionIndex}>
-                {condition.name ? (
-                  <p>
-                    If {condition.name}, go to {condition.section}
-                  </p>
-                ) : (
-                  <p>{condition.text}</p>
-                )}
-              </div>
-            ))
-          )}
-        </div>
-      ))}
-      <button onClick={() => setSelectedLocation(0)}>0</button>
+      <div>
+        <p>
+          {location.location}.{sectionNumber}
+        </p>
+        {section.text ? (
+          <p>{section.text}</p>
+        ) : (
+          section.condition?.map((condition, conditionIndex) => (
+            <div key={conditionIndex}>
+              {condition.name ? (
+                <p>
+                  If {condition.name}, go to{" "}
+                  <button onClick={() => goToSection(condition.section)}>
+                    {condition.section}
+                  </button>
+                </p>
+              ) : (
+                <p>{condition.text}</p>
+              )}
+            </div>
+          ))
+        )}
+        {location.data.length > 1 &&
+          Array.from({ length: location.data.length }, (_, i) => (
+            <button onClick={() => setSectionNumber(i)}>{i}</button>
+          ))}
+      </div>
+      <button onClick={() => goToSection("0.0")}>Back</button>
     </div>
   );
 }
---
+++ src/components/locations/location_53.ts
@@ -0,0 +1,17 @@
+const location53 = {
+  location: 53,
+  name: "SILARO MAKO, ARCHAEOLOGIST",
+  data: [
+    {
+      index: 1,
+      text: "You find Silaro, once again, deeply entranced in his work. This time though, he seems particularly intrigued by what looks like the shell of a giant power coupling protruding from a crack in the wall some two meters above him. He doesn’t seem to notice your approach until a twig cracks underneath your feet. “Oh!” he says, then relaxes. “It’s you. Actually, just the person I wanted to speak to! Well, really I’d love to speak with whoever engineered these pressure sensors, or better yet who removed them ... they appear to have been worn away shortly before the dam broke and the valley flooded.”",
+      rule: "Clear Silaro with  to ask if he needs any more help with his research.",
+    },
+    {
+      index: 6,
+      text: "As soon as you arrive, Silaro eyes a small patch of pebbled earth and quickly begins collecting samples. Worn from your journey, you finally rest in the nook of a hollowed tree trunk, which, for once, seems to hug every curve of your aching body. Just as you begin to drift off, you’re startled awake by Silaro’s joyous cry.",
+    },
+  ],
+};
+
+export default location53;
